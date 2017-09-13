c7b923e6281941d031bfaf126a52e910996e1d1f
Adam Fudge <coolfudge@gmail.com>
2017-09-13 17:39:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -150,7 +150,7 @@ def play(board)
   end
 
   if won?(board)
-    puts "Congratulations #{winner}!"
+    puts "Congratulations #{winner(board)}!"
   elsif draw?(board)
     puts "Cat's Game!"
   end
