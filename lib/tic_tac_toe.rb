3622ae69220a95b1e2263cbf47368fca20ea5eaa
Adam Fudge <coolfudge@gmail.com>
2017-09-13 17:10:10 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,29 +9,76 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
-
-def display_board
+def display_board (board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index
+def input_to_index(string_input)
+  int = string_input.to_i
+  index = int - 1
+  return index
 end
 
-def move
+def move(array, index, value)
+  array[index] = value
 end
 
-def position_taken?
+def position_taken? (board, index)
+  if board[index] == "X" || board[index] == "O"
+    return true
+  else
+    return false
+  end
 end
 
-def valid_move?
+def valid_move? (board, index)
+  if index >= 0 && index <= 8
+    if !position_taken?(board, index)
+      return true
+    end
+  else
+    return false
+  end
 end
 
-def turn
+def turn(board)
+  value = "X" #needs fixing
+
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+
+  if valid_move?(board, index)
+    move(board, index, value)
+
+    display_board(board)
+  else
+    puts "invalid"
+    turn(board)
+  end
 end
 
-def turn_count
+def turn_count(board)
+  count = 0
+  board.each do |markers|
+    if markers == "X" || markers == "O"
+      count +=1
+    end
+  end
+  return count
 end
 
-def current_player
+def current_player(board)
+  count = turn_count(board)
+  if count.even?
+    return "X"
+  else
+    return "O"
+  end
 end
 
 def won?(board)
