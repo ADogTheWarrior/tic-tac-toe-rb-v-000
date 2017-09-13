9fa47ad5ca16b49c06bdb8f3aa3118eb5e40cd87
Adam Fudge <coolfudge@gmail.com>
2017-09-13 17:07:06 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,43 @@
+WIN_COMBINATIONS
 
+def display_board
+end
+
+def input_to_index
+end
+
+def move
+end
+
+def position_taken?
+end
+
+def valid_move?
+end
+
+def turn
+end
+
+def turn_count
+end
+
+def current_player
+end
+
+def won?
+end
+
+def full?
+end
+
+def draw?
+end
+
+def over?
+end
+
+def winner
+end
+
+def play
+end
