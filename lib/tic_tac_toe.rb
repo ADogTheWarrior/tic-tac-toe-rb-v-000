083cdff932d7da47fec4b44813c613669d0bdc6d
Adam Fudge <coolfudge@gmail.com>
2017-09-13 17:08:16 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,4 +1,14 @@
-WIN_COMBINATIONS
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
 
 def display_board
 end
@@ -24,19 +34,67 @@ end
 def current_player
 end
 
-def won?
+def won?(board)
+  WIN_COMBINATIONS.each do |win_array|
+    win_index_0 = win_array[0]
+    win_index_1 = win_array[1]
+    win_index_2 = win_array[2]
+
+    value_at_win_index_0 = board[win_index_0]
+    value_at_win_index_1 = board[win_index_1]
+    value_at_win_index_2 = board[win_index_2]
+
+    if value_at_win_index_0 == "X" && value_at_win_index_1 == "X" && value_at_win_index_2 == "X"
+      return win_array
+    elsif value_at_win_index_0 == "O" && value_at_win_index_1 == "O" && value_at_win_index_2 == "O"
+      return win_array
+    end
+  end
+  false
 end
 
-def full?
+def full?(board)
+  full = true
+  board.each do |position|
+    if position != "X" && position != "O"
+      full = false
+      break
+    end
+  end
+  return full
 end
 
-def draw?
+def draw?(board)
+  if won?(board)
+    return false
+  else
+    if full?(board)
+      return true
+    else
+      return false
+    end
+  end
 end
 
-def over?
+def over?(board)
+  if draw?(board) || won?(board)
+    return true
+  else
+    return false
+  end
 end
 
-def winner
+def winner(board)
+  win_array = won?(board)
+  if !win_array
+    return nil
+  end
+
+  if board[win_array[0]] == "X"
+    return "X"
+  elsif board[win_array[0]] == "O"
+    return "O"
+  end
 end
 
 def play
